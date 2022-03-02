5654bfc67537cf7794d5ac34665a2b4aa9780fa8
github_mn <manjusha.nagarkar@gmail.com>
2022-03-02 14:35:17 +0530

    Jenkins Commit

--- src/com/cg/demo/JenkinsDemo.java
+++ src/com/cg/demo/JenkinsDemo.java
@@ -7,7 +7,8 @@ public class JenkinsDemo {
 	}
 
 	public static void main(String[] args) {
-	     System.out.println("Result : "+addNums(100,50));
+	    
+		System.out.println("Result : "+addNums(100,50));
 	}
 
 }
